8a8206e1b0121bfc48a8bf3483d9d920f878fa18
sudheer7898 <msudheer899@gmail.com>
2026-06-26 12:35:05 +0530

    Added constants

---
+++ utils/constants.py
@@ -0,0 +1,8 @@
+rootDir=r"C:\Users\CSIS workstation\OneDrive\Desktop\edo"
+num_frames=8
+transformer_blocks=2
+epochs=50
+save_interval=3
+DatasetPath=r"C:\Users\CSIS workstation\OneDrive\Desktop\edo\data"
+ValDatsetPath=r"C:\Users\CSIS workstation\OneDrive\Desktop\edo\data\CrackDataVal"
+batch_size=16
